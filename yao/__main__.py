8d2a09f0e1cbc277ce41b1dfce49d3d91fa674eb
José Sánchez-Gallego <gallegoj@uw.edu>
2022-04-01 14:17:02 -0700

    Initial version

--- yao/__main__.py
+++ yao/__main__.py
@@ -3,5 +3,13 @@
 #
 # @Author: José Sánchez-Gallego (gallegoj@uw.edu)
 # @Date: 2022-04-01
-# @Filename: conftest.py
+# @Filename: __main__.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ yao/exceptions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-04-01
+# @Filename: exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+class YaoError(Exception):
+    """A custom core Yao exception"""
+
+    def __init__(self, message=None):
+
+        message = "There has been an error" if not message else message
+        super(YaoError, self).__init__(message)
+
+
+class YaoNotImplemented(YaoError):
+    """A custom exception for not yet implemented features."""
+
+    def __init__(self, message=None):
+
+        message = "This feature is not implemented yet." if not message else message
+        super(YaoNotImplemented, self).__init__(message)
+
+
+class YaoMissingDependency(YaoError):
+    """A custom exception for missing dependencies."""
+
+    pass
+
+
+class YaoWarning(Warning):
+    """Base warning for Yao."""
+
+    pass
+
+
+class YaoUserWarning(UserWarning, YaoWarning):
+    """The primary warning class."""
+
+    pass
